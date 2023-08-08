a708b6e1369b4ab6c4be135360cf5254c9a5307c
museali <museali_@hotmail.co.uk>
2023-08-08 22:38:24 +0100

    Appuser, registration and security implemented

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -15,16 +16,45 @@ import java.util.Collections;
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails {
 
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private long id;
     private String name;
     private String username;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+
+    public AppUser(
+        String name,
+        String username,
+        String email,
+        String password,
+        AppUserRole appUserRole,
+        Boolean locked,
+        Boolean enabled) {
+            this.name = name;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.appUserRole = appUserRole;
+            this.locked = locked;
+            this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
